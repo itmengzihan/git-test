7855e2b9ca6e4980f7fa6ceafc5036485742f0b2
小孟 <764578946@qq.com>
2018-12-18 23:48:21 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,10 @@
+package com.itheima.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println(111);
+        System.out.println(222);
+        System.out.println(333);
+        System.out.println(444);
+    }
+}
